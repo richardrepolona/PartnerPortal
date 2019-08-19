e538637ed9a8bd5e101b71927c92111579c06b18
richard.repolona <richard.repolona@MIS-96MQBV2.novare.com.hk>
2019-08-19 09:38:26 +0800

    Chard

---
+++ Test Suites/Login/Login.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>395896f7-2c83-4dff-9227-b7dcc1df8f36</testSuiteGuid>
+   <testCaseLink>
+      <guid>555820b6-cce4-4c10-ac91-beb49fe80c6c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Partner Portal/RegistrationLoginPage/Login</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>4b9c94eb-e244-4413-bea8-d111dfed0093</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Credentials/Login</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>4b9c94eb-e244-4413-bea8-d111dfed0093</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>d09651a9-8166-4bba-ab02-ffd15b258228</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>4b9c94eb-e244-4413-bea8-d111dfed0093</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>9b1b1a5f-e2b0-481b-a65d-a02e06bfe397</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
